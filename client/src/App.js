4dde0a2b1d32162df52b0c6eaeb7677870939469
whishad <coderlweer@gmail.com>
2023-09-01 22:41:04 +0400

    (client) changing react base files

--- client/src/App.js
+++ client/src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is just the beginning
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/whishad/LDE"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Read About LDE
         </a>
       </header>
     </div>
