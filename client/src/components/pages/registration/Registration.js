834fa8e9cf4a56e51c67b67f53c39cec036c56ce
whishad <coderlweer@gmail.com>
2023-09-03 18:32:33 +0400

    (client) mаке registration page layout

--- client/src/components/pages/registration/Registration.js
+++ client/src/components/pages/registration/Registration.js
@@ -1,5 +1,12 @@
+import styles from './Registration.module.css'
+
 function Registration(){
-    return <p>this is the registration page</p>
+    return ( 
+        <div className={styles["registration-box"]}>
+            <input name="username" type="text" placeholder="your name" className={styles["input"]}/>
+            <button className={styles["btn"]}>Register</button>
+        </div>
+    )
 }
 
 export default Registration
