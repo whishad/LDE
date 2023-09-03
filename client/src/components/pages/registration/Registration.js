e862f4be24ae5f4f6c350e620fdd560b843d8a9a
whishad <coderlweer@gmail.com>
2023-09-03 23:22:25 +0400

    (client) connect input value with state, and analyze it by criteria

--- client/src/components/pages/registration/Registration.js
+++ client/src/components/pages/registration/Registration.js
@@ -1,10 +1,23 @@
+import { useState } from 'react'
 import styles from './Registration.module.css'
 
 function Registration(){
+    const [inpval, setVal] = useState("") // state to store input value 
+
+    const handleInputChange = (e) => { // state value update function
+        setVal(e.target.value)
+    }
+
+    const analyzeInputValue = () => { // checks if the input text matches the criteria
+        if(inpval.trim().length === 0) return false
+        if(inpval.length > 15) return false
+        return true
+    }
+
     return ( 
         <div className={styles["registration-box"]}>
-            <input name="username" type="text" placeholder="your name" className={styles["input"]}/>
-            <button className={styles["btn"]}>Register</button>
+            <input name="username" type="text" placeholder="your name" value={inpval} onChange={handleInputChange} className={styles["input"]}/>
+            <button className={styles["btn"]} onClick={() => analyzeInputValue() && console.log("text has been validated")}>Register</button>
         </div>
     )
 }
