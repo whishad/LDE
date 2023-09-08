99499661222ad8fd6e3e1d1b4d0879dfeeea3195
whishad <coderlweer@gmail.com>
2023-09-08 18:04:21 +0400

    (client) add new Route with messenger path, and write functionality by ternary operator that choosing navigate where in not registered paths

--- client/src/routes/Router.js
+++ client/src/routes/Router.js
@@ -1,13 +1,16 @@
 import { Navigate, Route, Routes } from "react-router";
 import { lazy } from "react";
+import { getFromLocalStorage } from "../globalfunctions/local_storarge_functions";
 
 const Registration = lazy(() => import("../components/pages/registration/Registration"))
 
 function Router(){
+    const navigation_address = getFromLocalStorage("username") ? "messenger" : "registration" 
     return (
         <Routes>
-            <Route path="*" element={<Navigate to="registration"/>}/>
+            <Route path="*" element={<Navigate to={navigation_address}/>}/>
             <Route path="registration" element={<Registration/>}/>
+            <Route path="messenger" element={<p>this is the messenger page</p>}/>
         </Routes>
     )
 }
