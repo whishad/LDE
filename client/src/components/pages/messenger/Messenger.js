8958b8f865def98651d8165a140c5ccdc5da75f0
whishad <coderlweer@gmail.com>
2023-09-17 22:22:54 +0400

    (client) write MessageForm tag structure and styles

--- client/src/components/pages/messenger/Messenger.js
+++ client/src/components/pages/messenger/Messenger.js
@@ -1,13 +1,16 @@
 import Message from "./components/Message"
 import MessageForm from "./components/MessageForm"
 import NavigationBar from "./components/NavigationBar"
+import styles from './Messenger.module.css'
 
 function Messenger(){
     return (
         <>
             {/* <Message/> */}
-            <NavigationBar/>
-            {/* <MessageForm/> */}
+            <div className={styles["messenger-box"]}>
+                <NavigationBar/>
+                <MessageForm/>
+            </div>
         </>
     )
 }
--- client/src/components/pages/messenger/components/MessageForm.js
+++ client/src/components/pages/messenger/components/MessageForm.js
@@ -1,6 +1,14 @@
+import styles from '../Messenger.module.css'
+import arrow_svg from '../../../../resources/images/send_message_button_arrow.svg'
+
 const MessageForm = () => {
     return (
-        <p>this is the message process</p>
+        <form className={styles["message-form"]}>
+            <input type="text" className={styles["message-input"]}/>
+            <button className={styles["message-button"]}>
+                <img src={arrow_svg} alt="send message"/>
+            </button>
+        </form>
     )
 }
 
