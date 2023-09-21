d23c55f1f788128e1181f6d3454c52055eaeb3b3
whishad <coderlweer@gmail.com>
2023-09-21 20:38:39 +0400

    (client) if the user is not in the chat/point, do not show him the message form and messages

--- client/src/components/pages/messenger/Messenger.js
+++ client/src/components/pages/messenger/Messenger.js
@@ -1,4 +1,6 @@
 import { useState } from "react"
+import { useParams } from "react-router"
+
 import Message from "./components/Message"
 import MessageForm from "./components/MessageForm"
 import NavigationBar from "./components/NavigationBar"
@@ -26,13 +28,16 @@ function Messenger(){
         },
     ])
 
+    const current_point = useParams() // getting current chat(point) name
+
     return (
         <div className={styles["messenger-box"]}>
             <NavigationBar points={points}/>
+            {current_point.point_name && 
             <div>
                 <Message messages={messages}/>
-            </div>
-            <MessageForm/>
+            </div>}
+            {current_point.point_name && <MessageForm/>}
         </div>
     )
 }
