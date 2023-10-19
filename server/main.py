e744b4ed1e7a61d61ff2c38aa32be8d43e52ec5d
whishad <coderlweer@gmail.com>
2023-10-19 16:51:24 +0400

    (server) create route /points, at the moment it just returns text

--- server/main.py
+++ server/main.py
@@ -2,5 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/points", methods = ["GET"])
+def points():
+    return "<h1> This page will return points json </h1>"
+
 if __name__ == "__main__":
     app.run(debug=True)
