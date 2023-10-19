a91bbc0d61e05ba00c6c6e233843d32a47a03f17
whishad <coderlweer@gmail.com>
2023-10-19 17:41:29 +0400

    (server) to get a response on the client without problems, create a function to configure the response cors

--- server/main.py
+++ server/main.py
@@ -2,6 +2,13 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+def configure_cors(obj, domain, methods, heads):
+    return ( 
+        obj.headers.add("Access-Control-Allow-Origin", domain), 
+        obj.headers.add("Access-Control-Allow-Methods", methods), 
+        obj.headers.add("Access-Control-Allow-Headers", heads) 
+    )
+
 @app.route("/points", methods = ["GET"])
 def points():
     if request.method == "GET":
@@ -16,6 +23,7 @@ def points():
             },
         ]
         response = jsonify(points)
+        configure_cors(response, "http://localhost:3000", "GET", "Content-Type")
         return response
 
 if __name__ == "__main__":
