200e93dbb8b9ceeebe96bc5fee934ebac13ca7c7
whishad <coderlweer@gmail.com>
2023-10-19 17:19:29 +0400

    (server) write if that check the request method is get, if so, then create and return a response from the list of dicts by use jsonify function

--- server/main.py
+++ server/main.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route("/points", methods = ["GET"])
 def points():
-    return "<h1> This page will return points json </h1>"
+    if request.method == "GET":
+        points = [
+            {
+                "point_name": "point1",
+                "id": "1"
+            },
+            {
+                "point_name": "point2",
+                "id": "2"
+            },
+        ]
+        response = jsonify(points)
+        return response
 
 if __name__ == "__main__":
     app.run(debug=True)
