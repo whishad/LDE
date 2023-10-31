08fb8872afa553bf42062650be19a339abc49783
whishad <coderlweer@gmail.com>
2023-10-31 20:49:22 +0400

    (client) create a state and the usual event handlers to get the input value

--- client/src/components/pages/messenger/components/MessageForm.js
+++ client/src/components/pages/messenger/components/MessageForm.js
@@ -1,10 +1,27 @@
 import styles from '../Messenger.module.css'
 import arrow_svg from '../../../../resources/images/send_message_button_arrow.svg'
+import { useState } from 'react'
 
 const MessageForm = () => {
+    const [inputValue, setInputValue] = useState("")
+
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setInputValue("")
+    }
+
     return (
-        <form className={styles["message-form"]}>
-            <input type="text" className={styles["message-input"]}/>
+        <form onSubmit={handleSubmit} className={styles["message-form"]}>
+            <input 
+            type="text"
+            value={inputValue}
+            onChange={handleInputChange}
+            className={styles["message-input"]}
+            />
             <button className={styles["message-button"]}>
                 <img src={arrow_svg} alt="send message"/>
             </button>
