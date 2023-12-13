4b235dfa4e7621cf5f09754a2929259a9d3fd8b4
whishad <coderlweer@gmail.com>
2023-12-14 00:01:08 +0400

    (client) send post request with message and message info to server in MessageForm.js

--- client/src/components/pages/messenger/components/MessageForm.js
+++ client/src/components/pages/messenger/components/MessageForm.js
@@ -1,6 +1,9 @@
 import styles from '../Messenger.module.css'
 import arrow_svg from '../../../../resources/images/send_message_button_arrow.svg'
 import { useState } from 'react'
+import { useParams } from 'react-router'
+import { sendRequest } from '../../../../globalfunctions/request_functions'
+import { getFromLocalStorage } from '../../../../globalfunctions/local_storarge_functions'
 
 const MessageForm = () => {
     const [inputValue, setInputValue] = useState("")
@@ -9,8 +12,21 @@ const MessageForm = () => {
         setInputValue(e.target.value)
     }
 
+    const current_point = useParams()
+
     const handleSubmit = (e) => {
         e.preventDefault()
+        sendRequest("POST", `http://localhost:5000/points/${current_point.point_name}`, {
+            author: getFromLocalStorage("username"),
+            author_pic_color: "red",
+            message_type: "text",
+            content: inputValue,
+            sent_time: (() => {
+                const current_date = new Date()
+                return `${current_date.getHours()}:${current_date.getMinutes()}`
+            })(),
+            id: 1,
+        }, 'application/json')
         setInputValue("")
     }
 
