12c1621cf69e5cc486b72fe722857f9514a50ce3
whishad <coderlweer@gmail.com>
2023-12-24 14:49:46 +0400

    (client) in the NavigationBar component write a buttonIsActive function that changes the color of the desired button so that it is clear which chat/point the user is in

--- client/src/components/pages/messenger/components/NavigationBar.js
+++ client/src/components/pages/messenger/components/NavigationBar.js
@@ -1,12 +1,21 @@
-import { NavLink } from 'react-router-dom'
+import { NavLink, useParams } from 'react-router-dom'
 import styles from '../Messenger.module.css'
 
 const NavigationBar = ({points}) => {
+
+    const current_point = useParams() // getting current chat(point) name
+
+    const buttonIsActive = (button_point_name) => { // Activates/changes button background-color by returning a css class
+        if(current_point.point_name == button_point_name){
+            return "active-point-link-button"
+        }
+    }
+
     return (
         <div className={styles["navbar"]}>
             {points.map(point => 
                 <NavLink key={point.id} to={`http://localhost:3000/messenger/${point.point_name}`}>
-                    <button className={styles["point-link-button"]}>
+                    <button className={`${styles["point-link-button"]} ${styles[buttonIsActive(point.point_name)]}`}>
                         {point.point_name}
                     </button>
                 </NavLink>
